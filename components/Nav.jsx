38112ba0193402b8dfa3548e1f73aa8000cd0118
ayeryn <erynli@utexas.edu>
2024-06-27 15:38:53 -0500

    Nav - add signedin view

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,9 +6,11 @@ import { useState, userEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href="/" className='flex gap-2 flex-center'>
+      <Link href='/' className='flex gap-2 flex-center'>
       <Image 
       src="/assets/images/logo.svg" 
       alt="Promptopia.logo"
@@ -16,7 +18,38 @@ const Nav = () => {
       height={30}
       className='object-contain'
       />
+
+      <p className='logo_text'>Promptopia</p>
       </Link>
+
+      {/* Mobile Navigation */}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt'
+            className='black_btn'>
+              Create Prompt
+            </Link>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href='/profile'>
+              <Image 
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )
+        }
+      </div>
     </nav>
   )
 }
