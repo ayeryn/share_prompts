878515a1f0c0d516810e64807dacd9a0a9570c8f
ayeryn <erynli@utexas.edu>
2024-06-27 15:41:56 -0500

    add providers

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,11 +2,23 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState, userEffect } from 'react';
+import { useEffect, useState, userEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  }, [])
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
