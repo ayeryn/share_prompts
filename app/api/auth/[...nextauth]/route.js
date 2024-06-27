c1a7d0667e883aa283748b5c08c9420679cc7b26
ayeryn <erynli@utexas.edu>
2024-06-27 17:32:27 -0500

    signIn

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -17,8 +18,18 @@ const handler = NextAuth({
       await connectToDB();
 
       // Check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
 
       // If not create a new user and save to DB
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
     } catch (error) {
       console.log(error);
     }
