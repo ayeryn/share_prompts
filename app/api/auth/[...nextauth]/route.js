ef09c654e6545ad5fead810768d4f6ff4255d8dc
ayeryn <erynli@utexas.edu>
2024-06-27 17:34:13 -0500

    update session to reflect current user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,12 @@ const handler = NextAuth({
     }),
   ],
 
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
 
   async signIn({ profile }) {
     try {
