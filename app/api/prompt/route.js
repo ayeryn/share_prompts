7728014b4b6df9ea8335ca77ddbb22b54d60fb2b
ayeryn <erynli@utexas.edu>
2024-06-28 12:55:28 -0500

    get posts

---
+++ app/api/prompt/route.js
@@ -0,0 +1,18 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    // console.error("Error fetching posts:", error);
+    return new Response("Failed to fetch all posts!", {
+      status: 500,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+};
