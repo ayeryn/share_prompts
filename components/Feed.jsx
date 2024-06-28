bc51d1cfec35954790a4ba4cda06a333041f0f15
ayeryn <erynli@utexas.edu>
2024-06-28 11:57:59 -0500

    implement PromptCardList

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,10 +3,28 @@
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  <div className="mt-16 prompt_layout">
+    {data.map((post) => (
+      <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+    ))}
+  </div>;
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt/");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+  }, []);
 
   return (
     <section className="feed">
@@ -20,6 +38,8 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
+
+      <PromptCard data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
