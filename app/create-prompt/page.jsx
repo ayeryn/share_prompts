33347e3cc46eae2f0a574e908c246cc0f511b0a1
ayeryn <erynli@utexas.edu>
2024-06-28 10:50:37 -0500

    createPrompt function

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -13,7 +13,29 @@ const CreatePrompt = () => {
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault(); // prevent form reloading
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
